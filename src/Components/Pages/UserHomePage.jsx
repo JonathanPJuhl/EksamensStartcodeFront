a78feb22f2b8455a724f23c0ebe21494b4eff97f
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-11-16 13:24:24 +0100

    profile page is now pretty functional

--- src/Components/Pages/UserHomePage.jsx
+++ src/Components/Pages/UserHomePage.jsx
@@ -1,65 +1,61 @@
 import { accountURL } from "../../settings";
 import React, { useState, useEffect } from "react";
-import handleHttpErrors from "../Errors/Errors";
 import { fetchUsername } from "../Authentication/decodeJWT";
 import { getToken, loggedIn } from "../Functionality/Login";
+import UpdateProfile from "../Functionality/UpdateProfile";
 
 export default function UserHomePage() {
+  
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-    const [accountInfo, setaccountInfo] = useState({email: "", profileText: ""});
-    
-    const makeOptions = (method, addToken, body) => {
-      var opts = {
-        method: method,
-        headers: {
-          "Content-type": "application/json",
-          Accept: "application/json",
-        },
-      };
-      if (addToken && loggedIn()) {
-        opts.headers["x-access-token"] = getToken();
-      }
-      if (body) {
-        opts.body = JSON.stringify(body);
-      }
-      return opts;
+  const [accountInfo, setaccountInfo] = useState({email: "", profileText: ""});
+
+  const makeOptions = (method, addToken, body) => {
+    var opts = {
+      method: method,
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
     };
+    if (addToken && loggedIn()) {
+      opts.headers["x-access-token"] = getToken();
+    }
+    if (body) {
+      opts.body = JSON.stringify(body);
+    }
+    return opts;
+  };
 
-    /*const fetchUserData = async () => {
-        const options = makeOptions("GET", true);
-        return fetch(accountURL + fetchUsername(), options)
-          .then(handleHttpErrors)
-           .then(setaccountInfo())
-      }; */
-      useEffect(() => {
-        const options = makeOptions("GET", true);
-        return fetch(accountURL + fetchUsername(), options)
-          .then(handleHttpErrors)
-          .then((res) =>setaccountInfo(res)) // res.json())
-          .then((data) => setaccountInfo(data)).then(console.log(accountInfo))
-      },[]);
-    
-      if( accountInfo === undefined)  {
-        return <div> still loading </div>
-      }
+  const fetchItems = async () => {
+    const options = makeOptions("GET", true);
+    const accData = await fetch(accountURL + fetchUsername(), options);
+    const accInfo = await accData.json();
+  
+    setaccountInfo(accInfo);
+  };
 
+  function HandleOnChange() {
+
+  }
+  
     return (
      
         <div>
-          { console.log(accountInfo) }
-          <form /*onChange={HandleOnChange}*/>
+          <form onChange={HandleOnChange}>
             <p>Username:</p>
-            {/* <input type="text" id="username" value = {accountInfo.username}></input> */}
-            <p>Answer to recoveryquestion: </p>
-            <input type="text" id="answerToSecurityQuestion"></input>
+            <input type="email" id="username" defaultValue = {accountInfo.email}></input> 
+            <p>Profile Text:</p>
+            <textarea type="text" id="profile-text" defaultValue = {accountInfo.profileText}></textarea>
             <br></br>
             <br></br>
-            <p>Forgot your password - or just want to reset it?</p>
             <input
               class="buttons"
               type="button"
-              value="Submit"
-              // onClick={fetchSearchData}
+              value="Update"
+              onClick={UpdateProfile}
               Reset
               password
             />
